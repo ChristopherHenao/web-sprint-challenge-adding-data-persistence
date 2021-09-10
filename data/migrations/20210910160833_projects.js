4892e0e00b1fd0e1e7c8ea21b6ace4c444ba3578
ChristoherHenao <cq.henao@gmail.com>
2021-09-10 12:40:14 -0400

    tables built

---
+++ data/migrations/20210910160833_projects.js
@@ -0,0 +1,59 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 200)
+            .notNullable()
+        table.boolean('project_completed')
+            .defaultTo(false)
+    })
+
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name', 200)
+            .notNullable()
+            .unique()
+    })
+
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_description', 300)
+            .notNullable()
+        table.boolean('task_completed')
+            .defaultTo(false)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+
+    .createTable('project_resources', table => {
+        table.increments('project_resource_id')
+        table.integer('task_id')
+            .unsigned()
+            .notNullable()
+            .references('task_id')
+            .inTable('tasks')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        table.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
